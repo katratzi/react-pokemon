3b945b5e9daadc521a217fb7b4f0527eb816e328
katratzi <ric_clifford@hotmail.com>
2021-04-22 14:07:25 +0100

    add material ui button to catch 'em'

--- src/components/InputArea.js
+++ src/components/InputArea.js
@@ -1,19 +1,32 @@
 import React, { useState } from 'react';
+import Button from '@material-ui/core/Button';
+
 
 function InputArea(props) {
 
+    // controlled state for input
     const [inputText, setInputText] = useState("");
-
-
 
     function updateText(event) {
         const newValue = event.target.value;
         setInputText(newValue);
     }
 
+    function handleClick(event) {
+        console.log(event);
+    }
+
+    // I don't want capitalize on the button
+    const buttonStyle = {
+        textTransform: "none",
+    }
+
     return <div>
         <h1>Input: {inputText}</h1>
         <input type="text" onChange={updateText} value={inputText}></input>
+        <p>
+            <Button onClick={handleClick} style={buttonStyle} variant="contained" color="primary">Catch 'em'</Button>
+        </p>
     </div>
 }
 
